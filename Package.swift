a1b9731390d2b2150dc48a8539b273b83c735355
Jamie Webb <jamie.webb@guardian.co.uk>
2019-12-31 12:40:25 +0000

    add Thrift as dependency

--- Package.swift
+++ Package.swift
@@ -21,7 +21,7 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(
             name: "mobile-apps-thrift-swift",
-            dependencies: []),
+            dependencies: ["Thrift"]),
         .testTarget(
             name: "mobile-apps-thrift-swiftTests",
             dependencies: ["mobile-apps-thrift-swift"]),
